d7ff49b26536ea8f9ce59c1f601492cfe3bd5877
arshdeep singh <arshdeep4909@gmail.com>
2022-01-25 19:47:45 -0500

    added everything except select option.

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
 //Selectors
-
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
-const todolist = document.querySelector(".todo-list");
-
-// document.querySelector(".todo-input") returns the frist element which has
-// a class of .todo-input form our html file.
-
-// Event Listeners
-
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+//Event Listener
 todoButton.addEventListener("click", addTodo);
-
-//when we click on todobutton then we would
-// want addTodo function to run
-
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 //Functions
-
 function addTodo(event) {
-  // prevent form form submitting(from refreshing)
   event.preventDefault();
-  //Tdo div
+
+  //toDo DIV
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
+  //create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>'; // adding this <i> tag inside the button
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  // CHECK TRASH BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>'; // adding this <i> tag inside the button
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //APPEND TO LIST
+  todoList.appendChild(todoDiv);
+
+  //clear Todo INPUT VALUE (once we add we want the input value to reset to nothing)
+  todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  //DELETE TODO
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //CHECK MARK
+
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
